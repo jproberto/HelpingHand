4230feeffad8532d707aa887e3e0890b6e8e2cec
Unknown <joaopsroberto@gmail.com>
2018-09-18 18:38:04 -0300

    Testes de troca de ambiente via configuração do application.properties

--- src/main/java/br/com/ap604/HelpingHand/core/AppConfig.java
+++ src/main/java/br/com/ap604/HelpingHand/core/AppConfig.java
@@ -1,5 +1,6 @@
 package br.com.ap604.HelpingHand.core;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -8,13 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @SpringBootApplication
 @Controller
 public class AppConfig {
+
+	@Value("${ambiente.ativo}")
+	private String ambiente;
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(AppConfig.class, args);
 	}
 
 	@RequestMapping("/")
 	String index() {
-		return "index";
+		System.out.println(ambiente);
+		return ambiente;
 	}
 
 }
